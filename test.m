2142dc62854fdca55e4ebd574c32a36207a15828
ErikBoesen <me@erikboesen.com>
2018-07-26 19:17:25 -0400

    Delete Objective C implementation

--- test.m
+++
@@ -1,6 +0,0 @@
-#include <Foundation/Foundation.h>
-#include <CoreWLAN/CoreWLAN.h>
-
-int main() {
-    CWInterface *cwInterface = [[CWInterface alloc] interfaceWithName:@"en1"]; // specify here the WiFi interface
-}
